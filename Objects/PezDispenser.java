42e8aef9000c9db79dbc226187397f5753a98a22
Nathan Sass <nsass711@gmail.com>
2015-09-21 21:37:25 -0700

    minor change

--- Objects/PezDispenser.java
+++ Objects/PezDispenser.java
@@ -1,6 +1,6 @@
 public class PezDispenser {
 	// Must match filename
-	private String mCharachterName; // Instance Variable
+	private String mCharachterName; // Instance variable
 
 	public PezDispenser(String charachterName) { // Constructor must be same as the class name
 		mCharachterName = charachterName;
