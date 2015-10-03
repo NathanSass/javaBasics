4af5b065815b23eec83e00c150699e110e6a7f5b
Nathan Sass <nsass711@gmail.com>
2015-10-03 11:40:35 -0700

    added hash map things

--- dataStructures/Example.java
+++ dataStructures/Example.java
@@ -2,6 +2,9 @@ import com.structurepackage.Treet;
 import com.structurepackage.Treets;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
+
 import java.util.Arrays;
 import java.util.Date;
 import java.util.TreeSet;
@@ -22,5 +25,30 @@ public class Example {
     System.out.printf("Hash tags: %s %n", allHashTags);
     System.out.printf("Mentions: %s %n", allMentions);
 
+    Map<String, Integer> hashTagCounts = new HashMap<String, Integer>();
+    for(Treet treet: treets) {
+    	for (String hashTag: treet.getHashTags()) {
+    		Integer count = hashTagCounts.get(hashTag);
+    		if (count == null) {
+    			count = 0;
+    		}
+    		count++;
+    		hashTagCounts.put(hashTag, count);
+    	}
+    }
+    System.out.printf("Hash tag counts: %s %n", hashTagCounts);
+
+    Map<String, List<Treet>> treetsByAuthor = new HashMap<String, List<Treet>>();
+    for (Treet treet: treets) {
+    	List<Treet> authoredTreets = treetsByAuthor.get(treet.getAuthor());
+    	if (authoredTreets == null) {
+    		authoredTreets = new ArrayList<Treet>();
+    		treetsByAuthor.put(treet.getAuthor(), authoredTreets)
+    	}
+    	authoredTreets.add(treet);
+    }
+
+    System.out.printf("Treets by author: %s %n ", treetsByAuthor);
+    System.out.printf("Treets by me: %s %n", treetsByAuthor.get("nathan"));
   }
 }
